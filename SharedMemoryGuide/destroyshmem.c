2bc207f307e67092dd5123baf3f001aa50c748e3
Tanya Prasad <f20181054@hyderabad.bits-pilani.ac.in>
2021-11-02 13:01:55 +0530

    yyet

---
+++ SharedMemoryGuide/destroyshmem.c
@@ -0,0 +1,34 @@
+//
+// Created by root on 02/11/21.
+//
+
+//
+// Created by root on 02/11/21.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "shared_memory.h"
+
+#define BLOCK_SIZE 4096
+
+int main(int argc,char *argv[])
+{
+    if(argc!=1)
+    {
+        printf("usage - %s no args",argv[0]);
+        return -1;
+    }
+   if(destroy_memory_block(FILENAME)){
+       printf("Destroyed block : %s\n",FILENAME);
+   }
+   else
+   {
+       printf("Couldnt destroy block %s\n",FILENAME);
+   }
+    return 0;
+}
+
+
